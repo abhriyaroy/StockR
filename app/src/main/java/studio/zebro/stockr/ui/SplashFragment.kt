beab43c234f6db8ab31b1267a6dc6fcb1733ee91
Abhriya <abhriya.roy@biofourmis.com>
2021-05-09 21:02:56 +0530

    Add animation end listener in splash screen

--- app/src/main/java/studio/zebro/stockr/ui/SplashFragment.kt
+++ app/src/main/java/studio/zebro/stockr/ui/SplashFragment.kt
@@ -1,5 +1,6 @@
 package studio.zebro.stockr.ui
 
+import android.animation.Animator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +9,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import studio.zebro.core.navigation.RecommendationModuleRoute
-import studio.zebro.stockr.R
+import studio.zebro.stockr.databinding.FragmentSplashBinding
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -16,18 +17,38 @@ class SplashFragment : Fragment() {
 
     @Inject
     internal lateinit var recommendationModuleRoute: RecommendationModuleRoute
+    private lateinit var splashFragmentBinding : FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        return FragmentSplashBinding.inflate(inflater)
+            .let {
+                splashFragmentBinding = it
+                it.root
+            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        recommendationModuleRoute.populateView(findNavController())
+        splashFragmentBinding.splashLottieAnim.apply {
+            playAnimation()
+            addAnimatorListener(object  : Animator.AnimatorListener{
+                override fun onAnimationStart(animation: Animator?) {
+
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    recommendationModuleRoute.populateView(this@SplashFragment.findNavController())
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {}
+
+                override fun onAnimationRepeat(animation: Animator?) {}
+
+            })
+        }
     }
 
 }
