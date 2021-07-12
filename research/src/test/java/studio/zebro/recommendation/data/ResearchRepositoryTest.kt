375015e5a2c1b7d09ed4f8fb72f29f4b702867be
Abhriya Roy <roy.abhriya@gmail.com>
2021-07-12 17:10:41 +0530

    Test research repository

--- research/src/test/java/studio/zebro/recommendation/data/ResearchRepositoryTest.kt
+++ research/src/test/java/studio/zebro/recommendation/data/ResearchRepositoryTest.kt
@@ -11,9 +11,12 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.InOrder
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.inOrder
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoMoreInteractions
 import retrofit2.Response
@@ -24,7 +27,9 @@ import studio.zebro.datasource.local.LocalPreferenceSource
 import studio.zebro.datasource.remote.ResearchRemoteSource
 import studio.zebro.datasource.util.Constants
 import studio.zebro.datasource.util.ErrorCodes
+import studio.zebro.recommendation.data.mapper.StockResearchEntityMapperTestHelper
 import studio.zebro.recommendation.testdataprovider.NiftyIndexesDayWiseDataModelDataProvider
+import studio.zebro.recommendation.testdataprovider.StockResearchDataModelProvider.getStockResearchDataModelList
 import studio.zebro.research.data.ResearchRepository
 import studio.zebro.research.data.mapper.NiftyIndexesDayEntityMapper.mapNiftyIndexesDayWiseDataModelToNiftyIndexesDayEntity
 import java.lang.IllegalStateException
@@ -43,6 +48,7 @@ class ResearchRepositoryTest {
     private lateinit var dispatcherProvider: DispatcherProvider
     private lateinit var researchRepository: ResearchRepository
     private val gson = Gson()
+    private lateinit var inOrder : InOrder
 
     @Before
     fun setup(){
@@ -53,9 +59,187 @@ class ResearchRepositoryTest {
             researchRemoteSource
         )
 
-        Mockito.`when`(serializerProvider.getGson()).thenReturn(gson)
-        Mockito.`when`(dispatcherProvider.getIoDispatcher()).thenReturn(Dispatchers.Default)
+        `when`(serializerProvider.getGson()).thenReturn(gson)
+        `when`(dispatcherProvider.getIoDispatcher()).thenReturn(Dispatchers.Default)
+
+        inOrder = inOrder(localPreferenceSource, researchRemoteSource)
     }
+
+    @Test
+    fun `should return new list of stock research on fetchStockResearch call success with isForceRefresh true`()
+    = runBlocking {
+        val listOfStockResearchDataModel = getStockResearchDataModelList()
+        val mappedList = listOfStockResearchDataModel.map {
+            StockResearchEntityMapperTestHelper.mapStockResearchRemoteDataModelToStockResearchEntity(it)
+        }
+        `when`(researchRemoteSource.getResearchFromKotakSecurities()).thenReturn(
+            Response.success(listOfStockResearchDataModel)
+        )
+
+        researchRepository.fetchStockResearch(true)
+            .collect {
+                assertEquals(mappedList, it)
+            }
+
+        verify(serializerProvider).getGson()
+        verify(dispatcherProvider).getIoDispatcher()
+        inOrder.verify(researchRemoteSource).getResearchFromKotakSecurities()
+        inOrder.verify(localPreferenceSource).saveStockKotakResearch(listOfStockResearchDataModel)
+    }
+
+    @Test
+    fun `should return exception on fetchStockResearch call failure due to getResearchFromKotakSecurities error response with isForceRefresh true`()
+            = runBlocking {
+        val errorResponseBody = ResponseBody.create(null, "{}")
+        `when`(researchRemoteSource.getResearchFromKotakSecurities()).thenReturn(
+            Response.error(Constants.ERROR_CODE_NOT_LOADED, errorResponseBody)
+        )
+
+        researchRepository.fetchStockResearch(true)
+            .catch { e->
+                assert(e is CustomError)
+                assertEquals(ErrorCodes.NETWORK_ERROR_CODE, (e as CustomError).errorModel!!.code)
+                assertEquals(ErrorCodes.NETWORK_ERROR_MESSAGE, (e as CustomError).errorModel!!.message)
+            }
+            .collect {  }
+
+        verify(serializerProvider).getGson()
+        verify(researchRemoteSource).getResearchFromKotakSecurities()
+        verify(dispatcherProvider).getIoDispatcher()
+        Unit
+    }
+
+    @Test
+    fun `should return exception on fetchStockResearch call failure due to getResearchFromKotakSecurities throwing exception with isForceRefresh true`()
+            = runBlocking {
+        `when`(researchRemoteSource.getResearchFromKotakSecurities()).thenThrow(IllegalStateException::class.java)
+
+        researchRepository.fetchStockResearch(true)
+            .catch { e->
+                assert(e is CustomError)
+                assertEquals(ErrorCodes.NETWORK_ERROR_CODE, (e as CustomError).errorModel!!.code)
+                assertEquals(ErrorCodes.NETWORK_ERROR_MESSAGE, (e as CustomError).errorModel!!.message)
+            }
+            .collect {  }
+
+        verify(serializerProvider).getGson()
+        verify(researchRemoteSource).getResearchFromKotakSecurities()
+        verify(dispatcherProvider).getIoDispatcher()
+        Unit
+    }
+
+    @Test
+    fun `should return new list of stock research on fetchStockResearch call success with isForceRefresh false`()
+            = runBlocking {
+        val listOfStockResearchDataModel = getStockResearchDataModelList()
+        val listOfCachedStockResearchDataModel = getStockResearchDataModelList()
+        val mappedList = listOfStockResearchDataModel.map {
+            StockResearchEntityMapperTestHelper.mapStockResearchRemoteDataModelToStockResearchEntity(it)
+        }
+        val mappedCacheList = listOfCachedStockResearchDataModel.map {
+            StockResearchEntityMapperTestHelper.mapStockResearchRemoteDataModelToStockResearchEntity(it)
+        }
+        `when`(researchRemoteSource.getResearchFromKotakSecurities()).thenReturn(
+            Response.success(listOfStockResearchDataModel)
+        )
+        `when`(localPreferenceSource.getSavedKotakStockResearch()).thenReturn(listOfCachedStockResearchDataModel)
+        var isCacheValueComing = true
+
+        researchRepository.fetchStockResearch(false)
+            .collect {
+                if(isCacheValueComing) {
+                    isCacheValueComing = false
+                    assertEquals(mappedCacheList, it)
+                } else {
+                    assertEquals(mappedList, it)
+                }
+            }
+
+        verify(serializerProvider).getGson()
+        verify(dispatcherProvider).getIoDispatcher()
+        inOrder.verify(localPreferenceSource).getSavedKotakStockResearch()
+        inOrder.verify(researchRemoteSource).getResearchFromKotakSecurities()
+        inOrder.verify(localPreferenceSource).saveStockKotakResearch(listOfStockResearchDataModel)
+    }
+
+    @Test
+    fun `should return exception on fetchStockResearch call failure due to getSavedKotakStockResearch throws exception with isForceRefresh false`()
+            = runBlocking {
+        `when`(localPreferenceSource.getSavedKotakStockResearch()).thenThrow(IllegalStateException::class.java)
+
+        researchRepository.fetchStockResearch(false)
+            .catch { e->
+                assert(e is CustomError)
+                assertEquals(ErrorCodes.NETWORK_ERROR_CODE, (e as CustomError).errorModel!!.code)
+                assertEquals(ErrorCodes.NETWORK_ERROR_MESSAGE, (e as CustomError).errorModel!!.message)
+            }
+            .collect {}
+
+        verify(serializerProvider).getGson()
+        verify(dispatcherProvider).getIoDispatcher()
+        inOrder.verify(localPreferenceSource).getSavedKotakStockResearch()
+        Unit
+    }
+
+    @Test
+    fun `should return exception on fetchStockResearch call failure due to getResearchFromKotakSecurities throws exception with isForceRefresh false`()
+            = runBlocking {
+        val listOfCachedStockResearchDataModel = getStockResearchDataModelList()
+        val mappedCacheList = listOfCachedStockResearchDataModel.map {
+            StockResearchEntityMapperTestHelper.mapStockResearchRemoteDataModelToStockResearchEntity(it)
+        }
+        `when`(localPreferenceSource.getSavedKotakStockResearch()).thenReturn(listOfCachedStockResearchDataModel)
+        `when`(researchRemoteSource.getResearchFromKotakSecurities()).thenThrow(IllegalStateException::class.java)
+
+        researchRepository.fetchStockResearch(false)
+            .catch { e->
+                assert(e is CustomError)
+                assertEquals(ErrorCodes.NETWORK_ERROR_CODE, (e as CustomError).errorModel!!.code)
+                assertEquals(ErrorCodes.NETWORK_ERROR_MESSAGE, (e as CustomError).errorModel!!.message)
+            }
+            .collect {
+                assertEquals(mappedCacheList, it)
+            }
+
+        verify(serializerProvider).getGson()
+        verify(dispatcherProvider).getIoDispatcher()
+        inOrder.verify(localPreferenceSource).getSavedKotakStockResearch()
+        inOrder.verify(researchRemoteSource).getResearchFromKotakSecurities()
+        Unit
+    }
+
+    @Test
+    fun `should return exception on fetchStockResearch call failure due to getResearchFromKotakSecurities return error response with isForceRefresh false`()
+            = runBlocking {
+        val listOfCachedStockResearchDataModel = getStockResearchDataModelList()
+        val mappedCacheList = listOfCachedStockResearchDataModel.map {
+            StockResearchEntityMapperTestHelper.mapStockResearchRemoteDataModelToStockResearchEntity(it)
+        }
+        val errorResponseBody = ResponseBody.create(null, "{}")
+        `when`(localPreferenceSource.getSavedKotakStockResearch()).thenReturn(listOfCachedStockResearchDataModel)
+        `when`(researchRemoteSource.getResearchFromKotakSecurities())
+            .thenReturn(Response.error(Constants.ERROR_CODE_NOT_LOADED, errorResponseBody))
+
+        researchRepository.fetchStockResearch(false)
+            .catch { e->
+                assert(e is CustomError)
+                assertEquals(ErrorCodes.NETWORK_ERROR_CODE, (e as CustomError).errorModel!!.code)
+                assertEquals(ErrorCodes.NETWORK_ERROR_MESSAGE, (e as CustomError).errorModel!!.message)
+            }
+            .collect {
+                assertEquals(mappedCacheList, it)
+            }
+
+        verify(serializerProvider).getGson()
+        verify(dispatcherProvider).getIoDispatcher()
+        inOrder.verify(localPreferenceSource).getSavedKotakStockResearch()
+        inOrder.verify(researchRemoteSource).getResearchFromKotakSecurities()
+        Unit
+    }
+
+    /**
+     * ---------------------------------------------------------------------------------------
+     */
 
     @Test
     fun `should return Nifty50 index on fetchNifty50Index call success`()= runBlocking {
@@ -65,7 +249,7 @@ class ResearchRepositoryTest {
             mapNiftyIndexesDayWiseDataModelToNiftyIndexesDayEntity(
                 niftyIndexesDayWiseDataModel
             )
-        Mockito.`when`(researchRemoteSource.getNifty50Index()).thenReturn(
+        `when`(researchRemoteSource.getNifty50Index()).thenReturn(
             Response.success(niftyIndexesDayWiseDataModel)
         )
 
@@ -100,7 +284,7 @@ class ResearchRepositoryTest {
     @Test
     fun `should return exception on fetchNifty50Index failure due to getNifty50Index call failure with Error Response`() = runBlocking {
         val errorResponseBody = ResponseBody.create(null, "{}")
-        Mockito.`when`(researchRemoteSource.getNifty50Index())
+        `when`(researchRemoteSource.getNifty50Index())
             .thenReturn(Response.error(Constants.ERROR_CODE_NOT_LOADED, errorResponseBody))
 
         researchRepository.fetchNifty50Index()
